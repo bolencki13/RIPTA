e088c74364c0553a6e8eebf0781a78d585706fbb
Brian Olencki (bolencki13) <bolencki13@gmail.com>
2016-10-26 18:38:36 -0400

    Initial Commit

---
+++ RIPTA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RIPTA
+//
+//  Created by Brian Olencki on 10/26/16.
+//  Copyright © 2016 Brian Olencki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
